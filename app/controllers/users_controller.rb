ed28baa43c3feb12c49c33b7c06ff8a5cb48d7d9
BOLNUT <bolnut3@gmail.com>
2020-08-07 11:48:03 +0900

    ユーザー編集機能の実装

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+
+  def edit
+    #@users = User.find(params[:id])  
+  end
+
+  def update
+    if current_user.update(user_params) #もしユーザーの更新ができたら
+      redirect_to root_path #redirect_toでトップページへ
+    else
+      render :edit #更新できない場合はeditのビューにいく
+    end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+
+end
