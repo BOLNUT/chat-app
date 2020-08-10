d648a60940a3ef39022b22f3455120120a4425bb
BOLNUT <bolnut3@gmail.com>
2020-08-10 11:05:30 +0900

    変化なし

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user! # ログイン済ユーザーのみにアクセスを許可し、ログインをしていないユーザーはログイン画面に遷移させます。
-  before_action :configure_permitted_parameters, if: :devise_controller? # 
+  before_action :configure_permitted_parameters, if: :devise_controller? 
 
   protected
 
