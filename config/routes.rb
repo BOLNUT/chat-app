d8b122b8dd3f2ea0d982e0440c3c0a7217518c4f
BOLNUT <bolnut3@gmail.com>
2020-08-10 19:34:22 +0900

    結合テストの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'messages/index'
-  root "rooms#index"
+  root 'rooms#index'
 
-  resources :users , only: [:edit, :update]
-  resources :rooms, only: [:new, :create, :destroy] do
-    resources :messages, only: [:index, :create ]
+  resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create,:destroy] do
+    resources :messages, only: [:index, :create]
   end
 end
